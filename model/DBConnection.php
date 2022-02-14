fc9d289d16ba8a0fbc26be3bf0aa58355b79055a
Gabriel Costanza <costanzag1999@gmail.com>
2022-02-14 22:25:24 +0100

    Connection to DB

--- model/DBConnection.php
+++ model/DBConnection.php
@@ -2,10 +2,20 @@
 
 class DBConnection {
 
-
-    public function __construct(){
-
-
+    /**
+     * Metodo que conecta con la base de datos y devuelve el handler para realizar acciones en esta
+     * @return PDO|void
+     */
+    private function connect(){
+        try {
+            $user   = "root";
+            $pwd    = "1234";
+            $host   = "localhost";
+            $dbname = "battlenet";
+            return new PDO('mysql:host='.$host.';dbname='.$dbname, $user, $pwd);
+        }catch (PDOException $e){
+            print "Error: ". $e->getMessage() . "<br/>";
+            die();
+        }
     }
-
 }
