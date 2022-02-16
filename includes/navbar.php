3125d1b1703b16b745576fb5f52ba29e1c7dfeae
Gabriel Costanza <costanzag1999@gmail.com>
2022-02-16 11:22:02 +0100

    FlexNavBar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="signup.php">Registro</a></li>
-    </ul>
+    <a class="navOption" href="index.php">Home</a>
+    <a class="navOption" href="login.php">Login</a>
+    <a class="navOption" href="signup.php">Registro</a>
+
+
 </nav>
