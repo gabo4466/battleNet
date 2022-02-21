3d00064f5689f52faed22bebf058d7c451daf408
Gabriel Costanza <costanzag1999@gmail.com>
2022-02-21 16:25:54 +0100

    NavBar update

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,15 @@
 <nav>
     <a class="navOption" href="index.php">Home</a>
     <a class="navOption" href="login.php">Login</a>
-    <a class="navOption" href="signup.php">Registro</a>
+    <?php
+        if (isset($_SESSION["userId"])){
+
+    ?>
+    <?php
+        }else{
+    ?>
+        <a class="navOption" href="signup.php">Registro</a>
+    <?php
+        }
+    ?>
 </nav>
