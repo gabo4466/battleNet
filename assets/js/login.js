ee850974eb83ecf74bfcd037582c050aaa4b249b
Gabriel Costanza <costanzag1999@gmail.com>
2022-03-15 10:15:35 +0100

    Poco de JS

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,3 +6,36 @@ if (error === "usernotfound"){
 }else if (error === "emptyinput"){
     alert("Debes llenar todos los campos.");
 }
+console.log("Sin error")
+function validateEmail(email){
+    console.log("Validando email")
+    let valid = false;
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+        valid = true;
+    }else{
+        valid = false;
+    }
+    return valid;
+}
+
+
+function validateLogin(){
+    const inputs = document.getElementsByTagName("input");
+    let valid = true;
+    let index = 0;
+    while(valid === true && index < inputs.length){
+        if (inputs[index].value === ""){
+            valid = false;
+
+        }
+        index++;
+    }
+    if (valid){
+        valid = validateEmail(document.getElementsByName("email")[0].value);
+        if (valid){
+            document.frmLogin.submit();
+        }
+    }else{
+        alert("Debes llenar todos los campos.")
+    }
+}
