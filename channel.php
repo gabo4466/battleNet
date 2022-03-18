dbc6f070c9f59b134c2910dbc52334a887d7bf94
FrancisGondolak <sirfrancisdegondolak@hotmail.es>
2022-03-18 19:38:50 +0100

    Creación de página Forum y Channel, con sus hojas de estilos. Dame fuerzas, Pegaso!!

---
+++ channel.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <?php
+    include "includes/head.php";
+    ?>
+    <link rel="stylesheet" href="styles/channel.css">
+</head>
+<body>
+
+
+<?php
+include "includes/header.php";
+include "includes/navbar.php";
+?>
+<section>
+
+    <div id="channels">
+        <!--
+        <a id="general" href="channel.php?forum=diablo">General</a>
+        <a id="tutoriales" href="channel.php?forum=warcraft">Tutoriales</a>
+        <a id="historia" href="channel.php?forum=wow">Historia</a>
+        <a id="parches" href="channel.php?forum=starcraft">Parches</a>
+        <a id="lfg" href="channel.php?forum=overwatch">Looking for Group</a>
+        <a id="raids" href="channel.php?forum=heroes">Raids</a>-->
+    </div>
+
+</section>
+<?php
+include "includes/footer.php";
+?>
+
+
+
+
+</body>
+</html>
---
+++ forum.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+    <?php
+    include "includes/head.php";
+    ?>
+    <link rel="stylesheet" href="styles/forum.css">
+</head>
+<body>
+
+
+<?php
+include "includes/header.php";
+include "includes/navbar.php";
+?>
+<section>
+
+    <div id="rules">
+        <p>NORMAS</p>
+    </div>
+    <div id="forum">
+        <a id="diablo" href="channel.php?forum=diablo">Diablo III</a>
+        <a id="warcraft" href="channel.php?forum=warcraft">Warcraft III</a>
+        <a id="wow" href="channel.php?forum=wow">World of Warcraft</a>
+        <a id="starcraft" href="channel.php?forum=starcraft">Starcraft II</a>
+        <a id="overwatch" href="channel.php?forum=overwatch">Overwatch</a>
+        <a id="heroes" href="channel.php?forum=heroes">Heroes of Storm</a>
+        <a id="heartstone" href="channel.php?forum=heartstone">Heartstone</a>
+    </div>
+
+</section>
+<?php
+include "includes/footer.php";
+?>
+
+
+
+
+</body>
+</html>
