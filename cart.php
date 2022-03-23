f33e563a0536a8ee5ed8715f8e1b69a54cbf3f2b
Gabriel Costanza <costanzag1999@gmail.com>
2022-03-23 18:05:10 +0100

    Avance carrito de compra

--- cart.php
+++ cart.php
@@ -3,6 +3,8 @@
     <?php
     include "includes/head.php";
     ?>
+    <link rel="stylesheet" href="styles/cart.css">
+    <script src="assets/js/cart.js"></script>
 </head>
 <body>
 
@@ -12,6 +14,29 @@ include "includes/header.php";
 include "includes/navbar.php";
 ?>
 <section>
+
+    <div class="table">
+        <div class="row tableHeader">
+            <div class="column">Producto</div>
+            <div class="column">Tipo</div>
+            <div class="column">Precio</div>
+        </div>
+        <div class="row">
+            <div class="column">
+                Columna con excesiva cantidad de texto
+            </div>
+            <div class="column">
+                Columna2
+            </div>
+            <div class="column">
+                Columna numero tres
+            </div>
+
+        </div>
+
+
+    </div>
+
 
 </section>
 <?php
